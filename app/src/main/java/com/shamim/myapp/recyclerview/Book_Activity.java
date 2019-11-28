42c98eded8e5c5b95ea5354b7bfe8149984b1f1e
MD. SHAMIM HOSSAIN <mdshamimh529@gmail.com>
2019-11-28 12:32:41 +0600

    Initial commit

---
+++ app/src/main/java/com/shamim/myapp/recyclerview/Book_Activity.java
@@ -0,0 +1,39 @@
+package com.shamim.myapp.recyclerview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Book_Activity extends AppCompatActivity {
+
+    private TextView tvtitle,tvdescription,tvcategory;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_);
+
+        tvtitle = (TextView) findViewById(R.id.txttitle);
+        tvdescription = (TextView) findViewById(R.id.txtDesc);
+        tvcategory = (TextView) findViewById(R.id.txtCat);
+        img = (ImageView) findViewById(R.id.bookthumbnail);
+
+        // Recieve data
+        Intent intent = getIntent();
+        String Title = intent.getExtras().getString("Title");
+       // String Description = intent.getExtras().getString("Description");
+        String des = intent.getExtras().getString("hello");
+        int image = intent.getExtras().getInt("Thumbnail") ;
+
+        // Setting values
+
+        tvtitle.setText(Title);
+        tvdescription.setText(des);
+        img.setImageResource(image);
+
+
+    }
+}
